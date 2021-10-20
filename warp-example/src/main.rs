e8614e253ca664e54bff34d4503a89f342536c36
armaangoel78 <armaangoel78@gmail.com>
2021-10-19 20:05:56 -0700

    changed to closures

--- warp-example/src/main.rs
+++ warp-example/src/main.rs
@@ -4,12 +4,16 @@ mod routes;
 #[tokio::main]
 async fn main() {
     let hello = warp::path!("hello" / String) 
-        .map(|name| format!("Hello, {}!", name)); 
-
-    let stream = warp::path!(String / "stream" / String).map(|application_id, stream_id| format!("{} and {}", application_id, stream_id));
+        .map(|name| {
+	   format!("Hello, {}!", name)
+	});
+    
+    let stream = warp::path!(String / "stream" / String)
+	.map(|application_id, stream_id| {
+	   format!("{} and {}", application_id, stream_id)
+	});
    
     let all = hello.or(stream); 
-
 
     warp::serve(all)
         .run(([0, 0, 0, 0], 8000))
